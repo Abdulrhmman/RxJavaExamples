cf35f55421a231dddfc95f465a4285ea2c80b903
Abdelrhman Ahmed <41817466+Abdulrhmman@users.noreply.github.com>
2020-01-05 03:59:09 +0200

    Update ClickTestActivity.java

--- app/src/main/java/com/abdelrahmman/rxjavaexamples/ClickTestActivity.java
+++ app/src/main/java/com/abdelrahmman/rxjavaexamples/ClickTestActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.RelativeLayout;
@@ -37,6 +39,8 @@ public class ClickTestActivity extends AppCompatActivity implements View.OnClick
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_click_test);
 
         relativeLayoutPrepare = findViewById(R.id.prepare_relative_layout);
